dcc3e200b653cf8ac8ccb6f928816f484c4ef4b0
gvibe3 <girmavibe@gmail.com>
2022-09-08 13:16:19 -0700

    taskC-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,13 +15,13 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of char:%d bytes\n", sizeof(a);
+printf("Size of char:%1d bytes\n", sizeof(a);
 
-printf("Size of  int:%d bytes\n", sizeof(b);
+printf("Size of  int:%1d bytes\n", sizeof(b);
 
-printf("Size of long int:%d bytes\n", sizeof(c);
+printf("Size of long int:%1d bytes\n", sizeof(c);
 
-printf("Size of long long int:%d byte\n", sizeof(d);
-printf("Size of float:%d byte\n", sizeof(e);
+printf("Size of long long int:%1d byte\n", sizeof(d);
+printf("Size of float:%1d byte\n", sizeof(e);
 return (0);
 }
