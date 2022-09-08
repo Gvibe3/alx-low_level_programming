fe768f37fba5d0082b00175c935f01b9f8ba8adc
gvibe3 <girmavibe@gmail.com>
2022-09-08 08:50:09 -0700

    taskC-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - comment here
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("/"Programming is like building a multilingual puzzle");
+return (0);
+
+}
