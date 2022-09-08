f42adc809f94ef111ec431f5b406191d282e30b9
gvibe3 <girmavibe@gmail.com>
2022-09-08 07:44:34 -0700

    taskC-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
-/**
- * main - ENTRY POINT
- *
- *RETURN: Always 0 (success)
- */
-int main (void)
+#include<stdio.h>
+int main(void)
 {
-  puts ( "\"Programming is like building a multilingual puzzle");
- 
-  return (0);
+puts("\Programming is like building a multilingual puzzle");
+return (0);
 }
