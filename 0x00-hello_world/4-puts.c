9b031822991c51785b4cf009fb501e1c6a80eab1
gvibe3 <girmavibe@gmail.com>
2022-09-08 08:32:14 -0700

    taskC-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always
  */
 
 int main(void)
 
 {
-puts(\"Programming is like building a multilingual puzzle)
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 
