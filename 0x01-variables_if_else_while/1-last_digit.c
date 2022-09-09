391613870c5bcc89e9ab242fcdfc58254661b691
gvibe3 <girmavibe@gmail.com>
2022-09-09 08:40:29 -0700

    taskV-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("last digit of %d and is greater than 5\n", n, 1);
 }
 else if (1 == 0)
 {
-printf("last digit of %d and 0\n", n, 1);
+printf("last digit of %d and is 0\n", n, 1);
 }
 
 else
