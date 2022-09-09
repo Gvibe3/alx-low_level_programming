b1046d5ee4a8e3495f8e94c3ba865e3cddcc14e4
gvibe3 <girmavibe@gmail.com>
2022-09-09 10:30:40 -0700

    taskV-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - alphabet in lowercase and uppercase
+*Return : Always 0
+*/
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+
+{
+
+putchar(c);
+
+}
+
+for (c = 'A'; c <= 'Z'; c++)
+
+{
+
+putchar(c);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
