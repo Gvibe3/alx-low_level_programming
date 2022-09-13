9ec7be12ffbcb33db03c67fb1c109a64b001f004
gvibe3 <girmavibe@gmail.com>
2022-09-13 07:54:23 -0700

    task-F4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isalpha - check lowercase character
+*@c: int for argument
+*Return: 1 if int c is lowercase, 0 if otherwise
+*/
+int _isalpha(int c)
+{
+for (c >= 'a' && c <= 'z')
+{
+return (1);
+else
+return (0);
+}
