0d45571c0396a104b06dc4800da0b4bc5558682e
gvibe3 <girmavibe@gmail.com>
2022-09-13 05:54:08 -0700

    task-F1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - prints the alphabet in lowercase
+*Return: Always 0
+*/
+void print_alphabet(void)
+{
+for (c = 'a'; c = 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
