c2287eb752bebe3f8370cc920cf449868fd3e467
gvibe3 <girmavibe@gmail.com>
2022-09-13 08:06:27 -0700

    task-F3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*_isalpha - check lowercase character
+*_islower - check lowercase character
 *@c: int for argument
 *Return: 1 if int c is lowercase, 0 if otherwise
 */
-int _isalpha(int c)
+int _islower(int c)
 {
 for (c >= 'a' && c <= 'z')
 {
