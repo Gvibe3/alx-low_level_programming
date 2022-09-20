1425019b70d8282358dd4891f6c49202f84f0b3c
gvibe3 <girmavibe@gmail.com>
2022-09-20 15:50:11 -0700

    task-P 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*swap_int -swap two intigers
+*@a : pointer to an int a
+*@b : pointer to an int b
+*Return : Always 0
+*/
+void swap_int(int *a, int *b);
+{
+int d;
+d = *a
+* a = *b
+* b = *f
+}
