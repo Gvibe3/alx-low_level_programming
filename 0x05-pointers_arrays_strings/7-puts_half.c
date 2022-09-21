efce6c5507124c2521b14d66f227e7d2f307fc36
gvibe3 <girmavibe@gmail.com>
2022-09-20 19:20:48 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*puts_half- put half of string
+*@str : string
+*Return: 0
+*/
+void puts_half(char *str)
+{
+int len = _strlen(str);
+if (len % 2 == 0)
+len = len / 2;
+else
+len = (len + 1) / 2;
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
+}
