1649e439b5bcde52081fba80672f4c1ad79d558b
gvibe3 <girmavibe@gmail.com>
2022-09-22 07:16:08 -0700

    task -3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 /**
-*_strcmp- function to compare strings
-*@s1: input first string
-*@s2: input second string
-*Return : to '0' if they are equal else to another number
+*_strcmp- function to copy strings
+*@s1: first string
+*@s2: second string
+*Return :int
 */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
-i++;
-return (*(s1 + i) -*(s2 + i));
+while (*s1 && *s1 == *s2)
+{
+s1++;
+s2++;
 }
-
+return (*s1 - *s2);
+}
