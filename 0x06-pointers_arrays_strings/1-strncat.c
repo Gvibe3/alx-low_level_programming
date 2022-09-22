34f7fd92f09df03e1e239054f4af90e6570d4c74
gvibe3 <girmavibe@gmail.com>
2022-09-22 06:18:39 -0700

    task -1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,14 +4,11 @@
 *_strncat- function to concatnates two string
 *@dest: output
 *@src: input
-*@n: byte for src
-*Return: two strings
+*@n: param3
+*Return: string
 */
 char *_strncat(char *dest, char *src, int n)
 {
-char destination[] = "Hello ";
-char source[] = "World!";
-strcat(destination, source);
-printf("Concatenated String: %s\n", destination);
-return (0);
+strncat(dest, src, n);
+return (dest);
 }
