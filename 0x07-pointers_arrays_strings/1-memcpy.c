8997b168a5ddb85c0eaf72c7ac9226b29587e321
gvibe3 <girmavibe@gmail.com>
2022-09-26 05:21:41 -0700

    task -1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-char *pdest = dest;
-char *src = src;
 for (i = 0; i < n; i++)
 {
-*pdest++ = *psrc++;
+*(dest + i) = *(src + i);
 }
 return (dest);
 }
