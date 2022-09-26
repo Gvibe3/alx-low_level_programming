f024184eed0834b071aafbf0fca305ada6e2c316
gvibe3 <girmavibe@gmail.com>
2022-09-26 08:23:46 -0700

    task- 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,16 +8,16 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i, j;
-int c = 0;
-int d = 0;
+int ascedent, descendet, i, sumAscedent, sumDescedent;
+sumAscedent = sumDescedent = 0;
+ascedent = 0;
+descendet = size - 1;
 for (i = 0; i < size; i++)
 {
-c = c + *(a + i * sizeof(int));
+sumAscedent += *(a + i * size + ascedent);
+sumDescedent += *(a + i * size + descendet);
+ascedent += 1;
+descendet -= 1;
 }
-for (j = 0; j < size; j++)
-{
-d = d + *(a + (size * j) + (size - j - 1));
-}
-printf("%d, %d\n", c, d);
+printf("%d, %d\n", sumAscedent, sumDescedent);
 }
